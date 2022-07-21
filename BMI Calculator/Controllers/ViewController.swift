6d7388247e997d50832ee34f0c0fab974af44af7
Alex Shabalin <shabalin.media@gmail.com>
2022-07-21 22:16:49 +0600

    Added IBActions and IBOutlets.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value * 100) / 100.0
+        hightLabel.text = "\(String(roundedValue)) cm"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
 
+        let roundedValue = Int(round(sender.value))
+        weightLabel.text = "\(String(roundedValue)) Kg"
+    }
 }
 
