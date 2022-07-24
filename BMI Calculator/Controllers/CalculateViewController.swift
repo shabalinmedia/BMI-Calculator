7e4cd11ec4d0ee9631ac4b3ccdfb9ae1570b5ce2
Alex Shabalin <shabalin.media@gmail.com>
2022-07-24 23:18:59 +0600

    Done.

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -37,8 +37,10 @@ class CalculateViewController: UIViewController {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController
             destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
-    
+     
 }
 
---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+
+}
